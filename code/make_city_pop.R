69cf6cc5032ccfdc9897cc086b3af7ec1f59464f
Thiel <pthiel@rwi-essen.de>
2023-08-15 14:03:26 +0200

    Change the merge method
    Aim of the file: adding population level to the specific city. Previous method
    was intersecting (through st_join) city shape and population grids. Problem:
    takes a lot of time. New method merge previously prepared city grids and grid
    population based on grid-id.

--- code/make_city_pop.R
+++ code/make_city_pop.R
@@ -1,57 +1,44 @@
-make_city_pop <- function(grid_pop, city_shapes) {
+make_city_pop <- function(grid_pop, city_grids) {
     #' @title Preparing city population
     #' 
     #' @description This function intersects the population grids with the
-    #' geographical information of cities in the project.
+    #' geographical information of cities in the project (on grid level).
     #' 
     #' @param grid_pop Grid populaton
-    #' @param city_shapes Shapes of included cities
+    #' @param city_grids Grids of included cities
     #' 
     #' @author Patrick Thiel
     #' @return QS file with all city grids
 
     #----------------------------------------------
-    # intersect city shapes and population grids
+    # merge city grids and population grids
 
-    # get all city names
-    cities <- unique(city_shapes$city_name)
+    city_pop_grids <- merge(
+        city_grids |> sf::st_drop_geometry(),
+        grid_pop,
+        by = "grid_id",
+        all.x = TRUE
+    )
 
-    # empty list for storage
-    city_grids_list <- list()
-
-    # loop through all cities
-    for (city in cities) {
-        # get city shape
-        city_shape <- city_shapes |>
-            dplyr::filter(city_name == city)
-
-        # intersect city shape with grids
-        suppressWarnings(
-            joint <- sf::st_join(
-                grid_pop,
-                city_shape,
-                left = TRUE,
-                largest = TRUE
-            )
+    # make sure that merged data set and input city data set have same length
+    tar_assert_true(
+        all.equal(
+            nrow(city_pop_grids),
+            nrow(city_grids)
         )
+    )
 
-        joint <- joint |>
-            dplyr::filter(!is.na(city_name)) |>
-            dplyr::relocate(city_name, .after = grid_id)
-
-        # store data
-        city_grids_list[[city]] <- joint
-    }
-
-    # combine all and set geometry
-    all_cities <- data.table::rbindlist(city_grids_list)
-    all_cities <- sf::st_set_geometry(all_cities, all_cities$geometry)
+    # set geometry
+    city_pop_grids <- sf::st_set_geometry(
+        city_pop_grids,
+        city_pop_grids$geometry    
+    )
 
     #----------------------------------------------
     # export
 
     qs::qsave(
-        all_cities,
+        city_pop_grids,
         file.path(
             data_path,
             "grids/population_grids_city.qs"
@@ -59,5 +46,5 @@ make_city_pop <- function(grid_pop, city_shapes) {
     )
 
     #----------------------------------------------
-    return(all_cities)
+    return(city_pop_grids)
 }
