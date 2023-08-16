7886697ccfd1908f7d0d63177f465d97b9fcbfa3
Patrick Thiel <patrickthiel@outlook.de>
2023-08-16 08:59:05 +0200

    Relative GDP
    Added a line that calculates the relative city GDP (relative to the real GDP of
    the Europe Union). To make the values more comparable.

--- code/aggregate_city_gdp.R
+++ code/aggregate_city_gdp.R
@@ -10,6 +10,17 @@ aggregate_city_gdp <- function(city_gdp) {
     #' @return File with city-level GDP
     
     #----------------------------------------------
+    # specify GDP per capita on Europe-level
+    # Eurostat Info "Real GDP per capita" (Code: SDG_08_10)
+    # info from: https://ec.europa.eu/eurostat/databrowser/view/sdg_08_10/default/table
+    # use info for 2015 European Union (with 28 states since UK was still member
+    # in 2015)
+    # 2015 because the city level data is from 2015 as well
+    # Accessed: 15.08.2023 (PT)
+    
+    europe_gdp <- 26710
+
+    #----------------------------------------------
     # aggregate GDP at city-level
 
     aggregated_gdp <- city_gdp |>
@@ -19,6 +30,9 @@ aggregate_city_gdp <- function(city_gdp) {
             total_gdp = sum(total_gdp, na.rm = TRUE),
             total_pop = sum(pop_2018, na.rm = TRUE),
             total_gdp_per_capita = total_gdp / total_pop
+        ) |>
+        mutate(
+            rel_total_gdp_per_capita = (total_gdp_per_capita / europe_gdp) * 100
         ) |>
         as.data.frame()
 
